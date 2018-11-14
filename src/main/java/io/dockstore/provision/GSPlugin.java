d8e94536812f4a253e4933a295e18f3bb0740f33
Walt Shands <jshands@ucsc.edu>
2018-11-14 15:35:18 -0800

    upload and download in chunks

--- src/main/java/io/dockstore/provision/GSPlugin.java
+++ src/main/java/io/dockstore/provision/GSPlugin.java
@@ -16,8 +16,16 @@
 package io.dockstore.provision;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Type;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
@@ -26,6 +34,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
+import com.google.cloud.ReadChannel;
+import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
@@ -108,6 +118,9 @@ public class GSPlugin extends Plugin {
             String bucketName = splitPathList.remove(0);
             String blobName = String.join(File.separator, splitPathList);
 
+            Blob blobMetadata = gsClient.get(bucketName, blobName, Storage.BlobGetOption.fields(Storage.BlobField.values()));
+            long inputSize = blobMetadata.getSize();
+
             Blob blob = null;
             try {
                 blob = gsClient.get(BlobId.of(bucketName, blobName));
@@ -116,7 +129,9 @@ public class GSPlugin extends Plugin {
                 return false;
             }
 
+/*
             if (blob != null) {
+
                 try {
                     blob.downloadTo(destination);
                 } catch (StorageException e) {
@@ -129,10 +144,65 @@ public class GSPlugin extends Plugin {
                         + "Please verify that the GCS bucket and path exist.");
                 return false;
             }
+*/
+            ProgressPrinter printer = new ProgressPrinter();
+
+            FileOutputStream fileContent = null;
+            try {
+                fileContent = new FileOutputStream((destination.toFile()));
+            } catch (FileNotFoundException e) {
+                System.err.println("File not found exception:" + e.getMessage());
+                return false;
+            }
+            PrintStream writeTo = new PrintStream(fileContent);
+
+            if (blob.getSize() < 1_000_000) {
+                // Blob is small read all its content in one request
+                byte[] content = blob.getContent();
+                try {
+                    writeTo.write(content);
+                } catch (IOException e) {
+                    System.err.println("Could not write download file. IO exception:" + e.getMessage());
+                    return false;
+                }
+
+            } else {
+                // When Blob size is big or unknown use the blob's channel reader.
+                try (ReadChannel reader = blob.reader()) {
+                    WritableByteChannel channel = Channels.newChannel(writeTo);
+                    ByteBuffer bytes = ByteBuffer.allocate(64 * 1024);
+                    try {
+                        int limit = 0;
+                        long runningTotal = 0;
+                        while ((limit = reader.read(bytes)) > 0) {
+                            bytes.flip();
+                            channel.write(bytes);
+                            bytes.clear();
+                            runningTotal = runningTotal + limit;
+                            printer.handleProgress(runningTotal, inputSize);
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Could not download file. IO exception:" + e.getMessage());
+                        return false;
+                    }
+
+                }
+            }
+
+            writeTo.close();
+            return true;
         }
 
         public boolean uploadTo(String destPath, Path sourceFile, Optional<String> metadata) {
             long inputSize = sourceFile.toFile().length();
+
+            String contentType = "application/octet-stream";
+            try {
+                contentType = Files.probeContentType(sourceFile);
+            } catch (IOException e) {
+                System.err.println("Could not get content type of file. Using default content type. IO exception:" + e.getMessage());
+            }
+
             Storage gsClient = getGoogleGSClient();
 
             String trimmedPath = destPath.replace("gs://", "");
@@ -144,6 +214,7 @@ public class GSPlugin extends Plugin {
             // Initialize BlobInfo to a default object
             //BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
             BlobInfo blobInfo = null;
+
 
             // To test metadata uncomment the lines below
             //Map<String,String> myMap = new HashMap<String, String>();
@@ -161,18 +232,18 @@ public class GSPlugin extends Plugin {
                     for (Map.Entry<String, String> entry : map.entrySet()) {
                         System.out.println("Loading " + entry.getKey() + "->" + entry.getValue());
                     }
-                    blobInfo = BlobInfo.newBuilder(blobId).setMetadata(map).build();
+                    blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).setMetadata(map).build();
                 } catch (com.google.gson.JsonSyntaxException ex) {
                     System.err.println(
                             "Could not load metadata. Metadata syntax exception. Uploading file without metadata:" + ex.getMessage());
-                    blobInfo = BlobInfo.newBuilder(blobId).build();
+                    blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
                 }
             } else {
-                blobInfo = BlobInfo.newBuilder(blobId).build();
+                blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
             }
 
             //putObjectRequest.setGeneralProgressListener(getProgressListener(inputSize));
-
+/*
             byte[] fileContent = null;
             try {
                 fileContent = Files.readAllBytes(sourceFile);
@@ -195,6 +266,48 @@ public class GSPlugin extends Plugin {
             } catch (StorageException e) {
                 System.err.println("Could not upload file. GCS storage upload exception:" + e.getMessage());
                 return false;
+            }
+            return true;
+*/
+
+
+            ProgressPrinter printer = new ProgressPrinter();
+
+            if (inputSize > 1_000_000) {
+                // When content is not available or large (1MB or more) it is recommended
+                // to write it in chunks via the blob's channel writer.
+                try {
+                    WriteChannel writer = gsClient.writer(blobInfo);
+                    byte[] buffer = new byte[1024];
+                    try (InputStream input = Files.newInputStream(sourceFile)) {
+                        int limit;
+                        long runningTotal = 0;
+                        while ((limit = input.read(buffer)) >= 0) {
+                            try {
+                                writer.write(ByteBuffer.wrap(buffer, 0, limit));
+                                runningTotal = runningTotal + limit;
+                                printer.handleProgress(runningTotal, inputSize);
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                                return false;
+                            }
+                        }
+                    }
+
+                } catch (IOException e) {
+                    System.err.println("Could not upload file. IO exception:" + e.getMessage());
+                    return false;
+                }
+            } else {
+                byte[] bytes = null;
+                try {
+                    bytes = Files.readAllBytes(sourceFile);
+                } catch (IOException e) {
+                    System.err.println("Could not read file to upload. IO exception:" + e.getMessage());
+                    return false;
+                }
+                // create the blob in one request.
+                gsClient.create(blobInfo, bytes);
             }
             return true;
         }
